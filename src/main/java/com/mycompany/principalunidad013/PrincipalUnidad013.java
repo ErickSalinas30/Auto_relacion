a120462c87d80d05aac10e0a704cd0c7e8d2acba
usuario <usuario@172.16.221.1>
2022-10-21 12:32:09 -0500

    mejoraDi

--- src/main/java/com/mycompany/principalunidad013/PrincipalUnidad013.java
+++ src/main/java/com/mycompany/principalunidad013/PrincipalUnidad013.java
@@ -13,7 +13,7 @@ public class PrincipalUnidad013 {
     
 
     public static void main(String[] args) {
-        
+/*        
 
         var arreglo1 = new Arreglo();
 
@@ -70,21 +70,60 @@ public class PrincipalUnidad013 {
     }
             
       
-    
+*/    
+  
+        var propietario1 = new Propietario();
+        propietario1.nombre="Juan Pérez";
+        propietario1.direccion="París y Oslo";
+        propietario1.telefono="0984357604";
+        propietario1.yearNacimiento=1981;
         
+        var propietario2 = new Propietario();
+        propietario2.nombre="Carlos López";
+        propietario2.direccion="Gran Colombia y Lamar";
+        propietario2.telefono="09843578596";
+        propietario2.yearNacimiento=1980;
+        
+        var lugar1 = new LugarMatriculacion();
+        lugar1.cuidad="Cuenca";
+        lugar1.descripcion="Centro de matriculacion de Capulispamba";
+        lugar1.provincia="Azuay";
+        lugar1.telefono="0965214835";
+        
+        var lugar2 = new LugarMatriculacion();
+        lugar2.cuidad="Gualaceo";
+        lugar2.descripcion="Centro de matriculacion vehicular de Gualaceo";
+        lugar2.provincia="Azuay";
+        lugar2.telefono="0958472531";
+        
+        
+        var auto1 = new Auto();
+        auto1.color="Amarillo";
+        auto1.cilindrajeMotor=1600;
+        auto1.marca="Toyota";
+        auto1.placa="AGF0526";
+        auto1.precio=18000;
+        auto1.year=2018;
+        auto1.owner=propietario2;
+        auto1.lugar=lugar1;
+        
+        
+        var auto2 = new Auto();
+        auto2.color="Azul";
+        auto2.cilindrajeMotor=2500;
+        auto2.marca="Chevrolet";
+        auto2.placa="UAF0526";
+        auto2.precio=18000;
+        auto2.year=2010;
+        auto2.owner=propietario1;
+        auto2.lugar=lugar2;
+        
+        System.out.println(auto1.mostrarInfo());
+        System.out.println(auto2.mostrarInfo());
     
-}
-
+    }
     
-            
-                
-           
-       
    
-        
-        
-        
 
-
-
-
+}      
+       
